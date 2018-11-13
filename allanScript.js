7dabe310effcd170503cac41ef106fbb179a335a
Allan Wilson <allan.wilson@vaisala.com>
2018-11-13 22:57:44 +0200

    first commit

---
+++ allanScript.js
@@ -0,0 +1,12 @@
+var now = 2018;
+var yearJohn = 1989;
+var fullAge = 18;
+
+var isFullAge = now - yearJohn >= fullAge; //comment
+console.log(isFullAge)
+
+var ageJohn = now - yearJohn;
+console.log("John is: " + ageJohn)
+var ageMark = 35;
+var average = ageJohn + ageMark / 2;
+console.log(average)
