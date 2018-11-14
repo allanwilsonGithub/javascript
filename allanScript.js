7cb506c37a656231f0337984a9acab8a95a62e0a
Allan Wilson <allan.wilson@vaisala.com>
2018-11-14 21:16:39 +0200

    Code Challenge 1

--- allanScript.js
+++ allanScript.js
@@ -1,3 +1,4 @@
+/*
 var now = 2018;
 var yearJohn = 1989;
 var fullAge = 18;
@@ -14,3 +15,29 @@ console.log(average)
 var x, y;
 x = y = (3 + 5) * 4 - 6; // 8 * 4 - 6 // 32 -6 // 26
 console.log(x, y);
+
+*/
+
+
+//Coding Challenge:
+
+var markHeight = prompt("What is Mark's height in metres?: ");
+console.log(markHeight + ' metres');
+var markMass = prompt("What is Mark's mass in kgs?: ");
+console.log(markMass + ' kgs');
+
+// Get Height's and Mass (the challenge said mass but I think it should be 'weight')
+var johnHeight = prompt("What is John's height in metres?: ");
+console.log(johnHeight + ' metres');
+var johnMass = prompt("What is John's mass in kgs?: ");
+console.log(johnMass);
+
+// Calculate BMI
+var markBMI = markMass / (markHeight * markHeight);
+var johnBMI = johnMass / (johnHeight * johnHeight);
+console.log(markBMI,johnBMI);
+
+// Who is a fat fecker?
+var isMarkBMIGreaterThanJohnBMI = markBMI > johnBMI;
+console.log("Mark's BMI is greater than John's: " + isMarkBMIGreaterThanJohnBMI);
+alert("Mark's BMI is greater than John's: " + isMarkBMIGreaterThanJohnBMI);
